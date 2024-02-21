e2a829a507db7ccec88754911be8d2148b2c536c
S00235076 <S00235076@atu.ie>
2024-02-21 12:31:31 +0000

    Completed

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import { MovieService } from './movie.service';
+
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { ListMoviesComponent } from './list-movies/list-movies.component' ;
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,AddMovieComponent, ListMoviesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'movie-list'
+}
---
+++ src/app/movie.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+
+  moviesList = [
+    { title: 'The Godfather', year: '1972', director: 'Francis Ford Coppola'},
+    { title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers'},
+    { title: 'Dial M For Murder', year: '1954', director: 'Alfred Hitchcock'},
+    { title: 'Whiplash', year: '2014', director: 'Damien Chazelle'}
+  ];
+
+  getMovies(){
+    return this.moviesList;
+  }
+  addMovie(movietitle:string,moviedirector:string,movieyear:string) {
+    this.moviesList.push({title:movietitle, director:moviedirector,year:movieyear});
+  }
+}
